0530703f59aba0728f6ffc95c0217d9268fdcbaf
sohamtapse <tapsesoham0@gmail.com>
2025-04-01 20:42:41 +0530

    done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -6,16 +6,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
