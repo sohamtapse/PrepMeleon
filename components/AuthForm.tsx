cb7b571c4f86cc2f727aab6ce11c40d5fbeee142
sohamtapse <tapsesoham0@gmail.com>
2025-04-05 21:06:14 +0530

    done with sign in and sign up page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -7,26 +7,46 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
 // import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  });
+};
 
 const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter();
+  const formSchema = authFormSchema(type);
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      password: "",
     },
   });
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    try {
+      if (type === "sign-up") {
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        toast.success("Sign in successfully.");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`There is an error: ${error}`);
+    }
   }
   const isSignIn = type === "sign-in";
   return (
@@ -34,18 +54,39 @@ const AuthForm = ({ type }: { type: FormType }) => {
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h2 className="text-primary-100">PrepMeleon</h2>
+          <h2 className="text-primary-100 ">PrepMeleon</h2>
         </div>
-        <h3>Practice your job interview, because you totally need it.</h3>
+        <h3 className="min-[566px]:text-xl">
+          Practice your job interview, because you totally need it.
+        </h3>
 
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
             className="space-y-6 w-full mt-4 form"
           >
-            {!isSignIn && <p>Name</p>}
-            <p>Email</p>
-            <p>Password</p>
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Enter Your Name"
+              />
+            )}
+            <FormField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="Enter Your Email"
+              type="email"
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Enter Your Password"
+              type="password"
+            />
             <Button className=" cursor-pointer btn" type="submit">
               {isSignIn ? "Sign In" : "Create an Account"}
             </Button>
@@ -55,7 +96,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
           {isSignIn ? "Make an account ASAP" : "Have an account already?"}
           <Link
             href={!isSignIn ? "/sign-in" : "/sign-up"}
-            className="font-bold text-user-primary ml-2"
+            className="font-bold text-user-primary ml-1"
           >
             {!isSignIn ? "Sign in" : "Sign up"}
           </Link>
