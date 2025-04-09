ae35cbdeb78321ba0606f272b1be926c797e2efe
sohamtapse <tapsesoham0@gmail.com>
2025-04-09 16:40:47 +0530

    home page done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex mx-auto max-w-7xl flex-col gap-12 my-10 px-16 max-sm:px-4 max-sm:my-8">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepMeleom</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 
 export default RootLayout;
